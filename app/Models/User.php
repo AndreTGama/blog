f8fae8f9162b1c28f89393f0c231ae1283db8dbb
AndreTGama <andre_gama789@hotmail.com>
2023-07-11 21:07:47 -0300

    new route login in API and function to autheticated

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    use HasFactory, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
@@ -24,7 +23,6 @@ class User extends Authenticatable
         'photo_name',
         'type_user_id',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -62,5 +60,23 @@ class User extends Authenticatable
         return Attribute::make(
             set: fn (string $value) => bcrypt($value),
         );
+    }
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UsersControllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +9,6 @@ Route::prefix('users')->group(function () {
     Route::post('/', [UsersControllers::class, 'store']);
 });
 
-
+Route::group(['middleware' => 'api','prefix' => 'auth'], function () {
+    Route::post('/', [AuthController::class, 'login']);
+});
