c1b8ecf4783b22161bbab1295cf8962a8abf2252
AndreTGama <andre_gama789@hotmail.com>
2023-07-27 21:21:29 -0300

    posts done

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Builder\ReturnMessage;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Posts\StorePostsRequest;
+use App\Http\Requests\Posts\UpdatePostsRequest;
 use App\Models\Posts;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
@@ -23,12 +24,12 @@ class PostsController extends Controller
 
         try {
             $data = $req->all();
-            $id = auth()->user()->id;
+            $authId = auth()->user()->id;
 
             $post = Posts::create([
                 'title' => $data['title'],
                 'post' => $data['post'],
-                'author_id' => $id
+                'author_id' => $authId
             ]);
 
             DB::commit();
@@ -46,6 +47,82 @@ class PostsController extends Controller
             return ReturnMessage::message(true, $e->getMessage(),$e->getMessage(),null, null, 400);
         }
 
+    }
+    /**
+     * update
+     *
+     * @param  UpdatePostsRequest $req
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function update(UpdatePostsRequest $req, int $id): JsonResponse
+    {
+        DB::beginTransaction();
+
+        try {
+            $data = $req->all();
+            $authId = auth()->user()->id;
+
+            $post = Posts::withTrashed()->where('id', $id)->first();
+
+            $post->update([
+                'title' => $data['title'],
+                'post' => $data['post'],
+                'aprove' => false,
+                'moderator_id' => null,
+                'author_id' => $authId
+            ]);
+
+            DB::commit();
+            return ReturnMessage::message(
+                false,
+                'Post created with success',
+                'Post created with success',
+                null,
+                $post,
+                200
+            );
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return ReturnMessage::message(true, $e->getMessage(),$e->getMessage(),null, null, 400);
+        }
+
+    }
+        /**
+     * desactive post in database
+     *
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function delete(int $id): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $post = Posts::findOrFail($id);
+            $post->delete();
+
+            DB::commit();
+
+            return ReturnMessage::message(
+                false,
+                'Post deleted with success',
+                'Post deleted with success',
+                null,
+                $post,
+                200
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
     }
     /**
      * getAll
@@ -84,6 +161,76 @@ class PostsController extends Controller
         }
     }
     /**
+     * getAllAuthPost
+     *
+     * @param  int $page
+     * @return JsonResponse
+     */
+    public function getAllAuthPost(int $page): JsonResponse
+    {
+        try {
+            $authId = auth()->user()->id;
+
+            $skip = ($page -1) * 12;
+            $posts = Posts::skip($skip)->take(12)->where(['author_id' => $authId])->get();
+
+            return ReturnMessage::message(
+                false,
+                'All posts make by author logged',
+                'All posts make by author logged',
+                null,
+                $posts,
+                200
+            );
+        } catch (\Exception $e) {
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
+    }
+    /**
+     * getOneAuthPost
+     *
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function getOneAuthPost(int $id): JsonResponse
+    {
+        try {
+            $authId = auth()->user()->id;
+
+            $post = Posts::where([
+                'id' => $id,
+                'author_id' => $authId
+            ])->first();
+
+            if(empty($post)) throw new \Exception('Post Not Found');
+
+            return ReturnMessage::message(
+                false,
+                'Post founded',
+                'Post founded',
+                null,
+                $post,
+                200
+            );
+        } catch (\Exception $e) {
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
+    }
+    /**
      * get
      *
      * @param  int $id
@@ -96,6 +243,8 @@ class PostsController extends Controller
                 'id' => $id,
                 'aprove' => true
             ])->first();
+
+            if(empty($post)) throw new \Exception('Post Not Found');
 
             return ReturnMessage::message(
                 false,
@@ -169,6 +318,123 @@ class PostsController extends Controller
                 200
             );
         } catch (\Exception $e) {
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
+    }
+    /**
+     * aprove
+     *
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function aprove(int $id): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $post = Posts::findOrFail($id);
+            if($post->aprove) throw new \Exception('Post has already been approved');
+            if(!$post->aprove && $post->moderator_id)
+                throw new \Exception('Post has already been desaproved');
+
+            $post->update([
+                'aprove' => true,
+                'moderator_id' => auth()->user()->id
+            ]);
+            DB::commit();
+
+            return ReturnMessage::message(
+                false,
+                'Post aproved',
+                'Post aproved',
+                null,
+                $post,
+                200
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
+    }
+    /**
+     * desaprove
+     *
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function desaprove(int $id): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $post = Posts::findOrFail($id);
+            if($post->aprove) throw new \Exception('Post has already been approved');
+            if(!$post->aprove && $post->moderator_id)
+                throw new \Exception('Post has already been desaproved');
+
+            $post->update([
+                'aprove' => false,
+                'moderator_id' => auth()->user()->id
+            ]);
+            DB::commit();
+
+            return ReturnMessage::message(
+                false,
+                'Post desaproved with success',
+                'Post desaproved with success',
+                null,
+                $post,
+                200
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return ReturnMessage::message(
+                true,
+                $e->getMessage(),
+                $e->getMessage(),
+                null,
+                null,
+                400
+            );
+        }
+    }
+    /**
+     * destroy post in database
+     *
+     * @param  int $id
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $post = Posts::withTrashed()->where('id',$id)->first();
+            $post->forceDelete();
+
+            DB::commit();
+
+            return ReturnMessage::message(
+                false,
+                'Post destroyed with success',
+                'Post destroyed with success',
+                null,
+                $post,
+                200
+            );
+        } catch (\Exception $e) {
+            DB::rollBack();
             return ReturnMessage::message(
                 true,
                 $e->getMessage(),
--- routes/Api/post/index.php
+++ routes/Api/post/index.php
@@ -17,15 +17,20 @@ Route::group([
     Route::group(['middleware' => 'auth.jwt'], function () {
 
         Route::post('/', 'store')->name('create');
-        Route::group(['prefix' => 'my-posts'], function () {
+        Route::put('/{id}', 'update')->name('update');
+        Route::delete('/{id}', 'delete')->name('delete');
 
+        Route::group(['prefix' => 'my-posts'], function () {
+            Route::get('/{page}', 'getAllAuthPost')->name('auth.all');
+            Route::get('/read/{page}', 'getOneAuthPost')->name('auth.one');
         });
 
         Route::group(['prefix' => 'moderator'], function () {
-            Route::get('/{page}', 'getAllModerator')->name('get.moderator.all');
-            Route::get('/read/{page}', 'getOneModerator')->name('get.moderator.one');
-            Route::put('/aprove/{id}', 'aprove')->name('get.moderator.aprove');
-            Route::put('/desaprove/{id}', 'desaprove')->name('get.moderator.desaprove');
+            Route::get('/{page}', 'getAllModerator')->name('moderator.all');
+            Route::get('/read/{page}', 'getOneModerator')->name('moderator.one');
+            Route::put('/aprove/{id}', 'aprove')->name('moderator.aprove');
+            Route::put('/desaprove/{id}', 'desaprove')->name('moderator.desaprove');
+            Route::delete('/destroy/{id}', 'destroy')->name('moderator.destroy');
         });
     });
 });
